8372b2e99612bdb2623107611390bf5939903380
akashdktyagi <akashdktyagi@gmail.com>
2019-12-29 16:07:36 +0530

    completed the test cases for task id 20

--- src/test/java/tc/TC_GetRequests.java
+++ src/test/java/tc/TC_GetRequests.java
@@ -1,11 +1,41 @@
 package tc;
 
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+import utils.Config;
+
+import static io.restassured.RestAssured.*;
 public class TC_GetRequests {
 
 	@Test
 	public void t_01_health_check() {
+		Response resp = given().baseUri(Config.BASE_URL).
+		when().get("/healthcheck").andReturn();
+		
+		int actual_status_code = resp.getStatusCode();
+		String body = resp.asString();
+		
+		Assert.assertEquals(actual_status_code, 200);
+		Reporter.log("Health Check is Successfull. with body as : " + body);
+			
 		
 	}
+	
+	@Test
+	public void t_02_get_request_for_product_with_id() {
+		Response resp = given().baseUri(Config.BASE_URL).
+		when().get("products/9132294").andReturn();
+		
+		int actual_status_code = resp.getStatusCode();
+		String body = resp.asString();
+		
+		Assert.assertEquals(actual_status_code, 200);
+		Reporter.log("Get Request for Product with Id is Successfull. with body as : " + body);
+			
+		
+	}
+	
 }
---
+++ src/test/java/utils/Config.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Config {
+
+	public static String BASE_URL = "http://localhost:3030/";
+	
+}
