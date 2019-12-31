bc13d38f75dd7f7fbf9b391dfe51bc7ad7f5d26c
Admin <pornima.alkari@gmail.com>
2019-12-31 23:05:34 +0530

    Added Config Class

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -1,7 +1,7 @@
 package utils;
 
-public class Config {
-
-	public static String BASE_URL = "http://localhost:3030/";
+public class Config
+{
+ public static String BASE_URL = "http://localhost:3030/";
 	
 }
