41b7fa7b7ce5297e3a4f8213c806c8081666c0c2
Admin <pornima.alkari@gmail.com>
2019-12-31 22:52:25 +0530

    Added TC_GetRequest_

--- src/test/java/tc/TC_GetRequests.java
+++ src/test/java/tc/TC_GetRequests.java
@@ -8,10 +8,11 @@ import io.restassured.response.Response;
 import utils.Config;
 
 import static io.restassured.RestAssured.*;
-public class TC_GetRequests {
-
+public class TC_GetRequests 
+{
 	@Test
-	public void t_01_health_check() {
+	public void t_01_health_check() 
+	{
 		Response resp = given().baseUri(Config.BASE_URL).
 		when().get("/healthcheck").andReturn();
 		
@@ -19,23 +20,21 @@ public class TC_GetRequests {
 		String body = resp.asString();
 		
 		Assert.assertEquals(actual_status_code, 200);
-		Reporter.log("Health Check is Successfull. with body as : " + body);
+		Reporter.log("Health Check is Successfull. with body as : " + body); //print body
 			
-		
 	}
 	
 	@Test
-	public void t_02_get_request_for_product_with_id() {
+	public void t_02_get_request_for_product_with_id() 
+	{
 		Response resp = given().baseUri(Config.BASE_URL).
-		when().get("products/9132294").andReturn();
+		when().get("products/9132294").andReturn(); //hit product
 		
 		int actual_status_code = resp.getStatusCode();
 		String body = resp.asString();
 		
-		Assert.assertEquals(actual_status_code, 200);
+		Assert.assertEquals(actual_status_code, 200); //checks code(validation)
 		Reporter.log("Get Request for Product with Id is Successfull. with body as : " + body);
-			
-		
 	}
 	
 }
